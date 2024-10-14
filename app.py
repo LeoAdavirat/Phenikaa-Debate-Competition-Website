d0c980809fe7dff5cc373096fcf54c96d8954e41
Leo Adavirat <52106425+LeoAdavirat@users.noreply.github.com>
2024-10-14 09:33:57 +0700

    Temporarily disabled server.

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
 from flask import Flask, render_template
-import socket
+import socket, random
 
 app = Flask(__name__)
 
-# Create a socket object to connect to the server
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# # Create a socket object to connect to the server
+# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to the server
-client_socket.connect(("localhost", 12345))
+# # Connect to the server
+# client_socket.connect(("localhost", 12345))
 
-# Receive the stage number from the server
-stage_number = client_socket.recv(1024).decode()
+# # Receive the stage number from the server
+# stage_number = client_socket.recv(1024).decode()
 
-@app.route("/")
-def index():
-    return render_template("garden.html")
-    # if stage_number == "1":
-    #     return render_template("garden.html")
-    # else:
-    #     return "Invalid stage number"
+# @app.route("/")
+# def index():
+#     return render_template("garden.html")
+#     # if stage_number == "1":
+#     #     return render_template("garden.html")
+#     # else:
+#     #     return "Invalid stage number"
 
-@app.route("/butterfly")
-def butterfly():
-    # Add a butterfly sprite image to the garden background
-    return render_template("butterfly.html")
+def add_butterfly(html_content):
+    butterfly_html = '<img src="{{ url_for("static", filename="images/butterfly_sprite.png") }}" style="position: absolute; left: {}px; top: {}px;">'.format(random.randint(0, 800), random.randint(0, 600))
+    return html_content.replace('</body>', butterfly_html + '</body>')
+
+@app.route('/')
+def home():
+    html_content = render_template('garden.html')
+    return add_butterfly(html_content)
 
 if __name__ == "__main__":
     app.run(debug=True)
